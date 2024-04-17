094c473b9215433e8d3d64c1f99117bd75f3eac1
unsubstantiated-Script <jusfulton@gmail.com>
2024-04-16 20:23:29 -0700

    Adding in the date

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-
 import pandas as pd
 import glob
 from fpdf import FPDF
@@ -7,14 +5,19 @@ from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
-
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
-    filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
-    pdf.set_font('Times', 'B', size=16)
-    pdf.cell(50, 8, txt=f"Invoice nr.{invoice_nr}")
-    pdf.output(f"PDFs/{filename}.pdf")
 
+    filename = Path(filepath).stem
+    invoice_nr, doc_date = filename.split("-")
+
+    pdf.set_font('Times', 'B', size=16)
+    pdf.cell(50, 8, txt=f"Invoice nr.{invoice_nr}", ln=1)
+
+    pdf.set_font('Times', 'B', size=16)
+    pdf.cell(50, 8, txt=f"Date: {doc_date}")
+
+    pdf.output(f"PDFs/{filename}.pdf")
